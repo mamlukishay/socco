4a0f1cf7693503b15949ea80ca054bad2ba8f113
Ishay Mamluk <ishay@appforma.com>
2012-11-28 22:13:50 +0200

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socco::Application.config.secret_token = 'bcc22a839fe5cbb39e12c883d656b0539e783ac02ccef59580ca3a0275328f48a474993ffeba96331c000e53f1ea8f98b00c02df475b58454211f35a04798943'
